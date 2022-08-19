e2cfec3d577181c91a4fe1dd74f31b144fb2a4fb
bjdgs <bjdgs@lilei.cn>
2022-08-19 21:26:35 +0800

    轮播图

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
+import Vue from 'vue'
 import App from './App'
 
-// #ifndef VUE3
-import Vue from 'vue'
-Vue.config.productionTip = false
-App.mpType = 'app'
-const app = new Vue({
-    ...App
-})
-app.$mount()
-// #endif
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
 
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
+uni.$http = $http
+
+// 请求的根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net/'
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+
+  // console.log(store)
+
+  // 判断当前请求的是否为有权限的接口
+  if (options.url.indexOf('/my/') !== -1) {
+    options.header = {
+      Authorization: store.state.m_user.token
+    }
   }
 }
-// #endif
+
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+// 封装弹框的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+  ...App
+})
+app.$mount()
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,12 @@
 <template>
   <view>
-    home
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item, i) in swiperList" :key="i">
+        <view class="swiper-item">
+          <img :src="item.image_src" alt="">
+        </view>
+      </swiper-item>
+    </swiper>
   </view>
 </template>
 
@@ -8,12 +14,35 @@
   export default {
     data() {
       return {
-        
+        swiperList: [],
       };
+    },
+    onLoad() {
+      this.getSwiperList()
+    },
+    methods: {
+      async getSwiperList() {
+        const { data: res } = await uni.$http.get('api/public/v1/home/swiperdata')
+        console.log(res)
+        if (res.meta.status !== 200) {
+          return uni.showToast({
+            title: '数据请求失败！',
+            duration: 1500,
+            icon: 'none'
+          })
+        }
+        this.swiperList = res.message
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+swiper {
+  height: 300rpx;
+  .swiper-item, image {
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>
